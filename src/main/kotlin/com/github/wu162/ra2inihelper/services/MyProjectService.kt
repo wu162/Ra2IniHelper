4dadae962aa25c60f7f9471b29b0a3725dfbd6ba
GitHub Action <action@github.com>
2024-12-06 11:54:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/wu162/ra2inihelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wu162/ra2inihelper/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.wu162.ra2inihelper.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wu162.ra2inihelper.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
